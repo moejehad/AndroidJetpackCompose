643c46d12571fb0126da3b94750e163cb42055e7
moejehad <mohammedmadhoun18@gmail.com>
2022-05-21 23:48:35 +0300

    Box

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -14,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,65 +24,38 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Column(
-                        modifier = Modifier
-                            .height(500.dp)
-                            .width(500.dp)
-                            .background(Color.LightGray),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        CustomItem(3f, MaterialTheme.colors.secondary)
-                        CustomItem(1f)
-                    }
-
-                    Row(
-                        modifier = Modifier
-                            .height(200.dp)
-                            .width(500.dp)
-                            .background(Color.DarkGray),
-                        horizontalArrangement = Arrangement.Start,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        CustomRowItem(3f, MaterialTheme.colors.secondary)
-                        CustomRowItem(1f)
-                    }
+                    Greeting()
                 }
             }
         }
     }
 }
 
-@Composable
-fun ColumnScope.CustomItem(weight: Float, color: Color = MaterialTheme.colors.primary) {
-    Surface(
-        modifier = Modifier
-            .width(200.dp)
-            .height(50.dp)
-            .weight(weight),
-        color = color
-    ) {}
-}
 
 @Composable
-fun RowScope.CustomRowItem(weight: Float, color: Color = MaterialTheme.colors.primary) {
-    Surface(
-        modifier = Modifier
-            .width(50.dp)
-            .height(50.dp)
-            .weight(weight),
-        color = color
-    ) {}
+fun Greeting() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Box(
+            modifier = Modifier
+                .background(Color.Blue)
+        ) {
+            Box(modifier = Modifier
+                .height(50.dp)
+                .width(50.dp)
+                .background(Color.Green))
+            Text(text = "I Love Android", fontSize = 20.sp)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackComposeTheme {
-
+        Greeting()
     }
 }
