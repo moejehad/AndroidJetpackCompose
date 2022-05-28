a7dd7b72afcbdbefa2ab4b4983ef38046c43d264
moejehad <mohammedmadhoun18@gmail.com>
2022-05-28 23:00:58 +0300

    GradientButton

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -12,76 +12,41 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
+import com.example.jetpackcompose.ui.theme.color1
+import com.example.jetpackcompose.ui.theme.color2
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeTheme {
-                Surface(
-                    color = MaterialTheme.colors.background
+                Row(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Greeting()
+                    GradientButton(
+                        text = "Button",
+                        textColor = Color.White,
+                        gradient = Brush.horizontalGradient(
+                            colors = listOf(
+                                color1,
+                                color2
+                            )
+                        ),
+                    ){
+
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(){
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        var password by rememberSaveable{
-            mutableStateOf("")
-        }
-        var passwordVisibilty by remember {
-            mutableStateOf(false)
-        }
-
-        val icon = if(passwordVisibilty)
-            painterResource(id = R.drawable.ic_launcher_foreground)
-        else
-            painterResource(id = R.drawable.ic_launcher_background)
-
-        OutlinedTextField(
-            value = password,
-            onValueChange = {
-                password = it
-            },
-            placeholder = { Text(text = "Password")},
-            label = { Text(text = "Password")},
-            trailingIcon = {
-                IconButton(onClick = {
-                    passwordVisibilty = !passwordVisibilty
-                }) {
-                    Icon(
-                       painter = icon, contentDescription = "Email"
-                    )
-                }
-            },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Password
-            ),
-            visualTransformation = if (passwordVisibilty) VisualTransformation.None
-            else PasswordVisualTransformation()
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackComposeTheme {
-
     }
 }
