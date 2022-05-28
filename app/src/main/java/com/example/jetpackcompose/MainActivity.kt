3a5292f7e0628d20ab3d080ea71586274dbe3b1a
moejehad <mohammedmadhoun18@gmail.com>
2022-05-28 23:21:15 +0300

    LazyColumn

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -3,49 +3,65 @@ package com.example.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Done
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcompose.repository.PersonRepository
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
-import com.example.jetpackcompose.ui.theme.color1
-import com.example.jetpackcompose.ui.theme.color2
 
 class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeTheme {
-                Row(
-                    modifier = Modifier.fillMaxSize(),
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    GradientButton(
-                        text = "Button",
-                        textColor = Color.White,
-                        gradient = Brush.horizontalGradient(
-                            colors = listOf(
-                                color1,
-                                color2
-                            )
-                        ),
-                    ){
+                /*val personRepository = PersonRepository()
+                val getAllData = personRepository.getAllData()
 
+                LazyColumn(
+                    contentPadding = PaddingValues(all = 20.dp),
+                    verticalArrangement = Arrangement.spacedBy(15.dp)
+                ){
+                    items(items = getAllData){ person ->
+                        CustomItem(person = person)
+                    }
+
+                }*/
+
+                val sections = listOf("A","B","C","G","E")
+
+                LazyColumn(
+                    contentPadding = PaddingValues(all = 20.dp),
+                    verticalArrangement = Arrangement.spacedBy(15.dp)
+                ){
+                    sections.forEach { section ->
+                        stickyHeader {
+                            Text(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(Color.LightGray)
+                                    .padding(12.dp),
+                                text = "Section $section"
+                            )
+                        }
+                        items(10){
+                            Text(
+                                modifier = Modifier.padding(12.dp),
+                                text = "item $it from the sction $section"
+                            )
+                        }
                     }
                 }
+
             }
         }
     }
